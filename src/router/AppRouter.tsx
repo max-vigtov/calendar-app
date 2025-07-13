ac7dc0199da850986cb1b3af282190a5dcc8429c
Max Vigueras <mvt.2000@hotmail.com>
2025-07-13 15:51:40 -0600

    Router: Routes config first draft

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth"
+import { CalendarPage } from "../calendar"
+
 export const AppRouter = () => {
+
+  const authStatus = 'no-authenticated'
   return (
-	<div>AppRouter</div>
+	<Routes>
+    {
+      (authStatus === 'no-authenticated') 
+      ? <Route path="/auth/*" element={<LoginPage />} />
+      : <Route path="/*" element={<CalendarPage />} />
+    }
+    <Route path="/*" element={<Navigate to="/auth/login" />} />
+  </Routes>
   )
 }
