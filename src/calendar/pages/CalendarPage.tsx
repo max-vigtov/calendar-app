bf4a8200ff582ff511b7cb2c998813a2e0623b0c
Max Vigueras <mvt.2000@hotmail.com>
2025-07-13 17:48:04 -0600

    Calendar: Calendar component created

--- src/calendar/pages/CalendarPage.tsx
+++ src/calendar/pages/CalendarPage.tsx
@@ -1,9 +1,48 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { format, parse, startOfWeek, getDay } from 'date-fns'
+import { enUS } from 'date-fns/locale/en-US'
 import { NavBar } from "../components/NavBar"
+import { addHours } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const myEventsList = [
+  {
+    title: 'Cumpleaños de Max',
+    notes: 'Comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Max',
+    },
+  },
+]
 
 export const CalendarPage = () => {
   return (
 	<>
 		<NavBar />
+
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc(100vh - 80px)' }}
+    />
   </>
   )
 }
