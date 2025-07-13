bdd50b47570cd12270a15556a350da4581b023cd
Max Vigueras <mvt.2000@hotmail.com>
2025-07-13 17:36:23 -0600

    Calendar: LoginPage & CalendarPage first draft

--- src/calendar/pages/CalendarPage.tsx
+++ src/calendar/pages/CalendarPage.tsx
@@ -1,5 +1,9 @@
+import { NavBar } from "../components/NavBar"
+
 export const CalendarPage = () => {
   return (
-	<div>CalendarPage</div>
+	<>
+		<NavBar />
+  </>
   )
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar"
 
 export const AppRouter = () => {
 
-  const authStatus = 'no-authenticated'
+  const authStatus = 'authenticated'
   return (
 	<Routes>
     {
